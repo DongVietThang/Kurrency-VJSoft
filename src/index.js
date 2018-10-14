5ccec94369ea25b752993be851d615d2605f0a1c
Thắng Đồng <dongvietthang1999@gmail.com>
2018-10-15 00:06:08 +0700

    Ver 2.0

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import Config from './route/Config';
 import GetStarted from '../src/screens/GetStarted';
 import Main from '../src/screens/Main';
 
-export default () => <Main/>
+export default () => <Config/>
